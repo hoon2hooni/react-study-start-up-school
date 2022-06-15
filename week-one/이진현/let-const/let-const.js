66c7302f1bdd587fcd6f3a8531733a3a0a2ce1c8
Jhlee1012 <leejinhyun327@gmail.com>
2022-06-15 17:16:55 +0900

    1주차 let-const 과제 제출

---
+++ week-one/이진현/let-const/let-const.js
@@ -0,0 +1,17 @@
+//퀴즈1
+"use strict";
+
+let admin ;
+let name ;
+name = "John";
+admin = name;
+alert(admin);
+
+//퀴즈2
+
+let ourPlanetName = "venus" ;
+let currnetUserName = "name";
+
+//퀴즈3
+const birthday = '18.04.1982';
+let age = someCode(birthday);
